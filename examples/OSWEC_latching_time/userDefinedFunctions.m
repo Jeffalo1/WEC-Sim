f6534f3442f92b08f0541c07573bb983577a4da6
Jeffalo1 <jeffg2@vt.edu>
2021-11-12 13:14:55 -0500

    Compare

--- examples/OSWEC_latching_time/userDefinedFunctions.m
+++ examples/OSWEC_latching_time/userDefinedFunctions.m
@@ -16,7 +16,57 @@ output.plotResponse(1,5);
 % Plot x forces for body 2
 output.plotForces(2,1)
 
-% Save waves and response as video
-% output.plotWaves(simu,body,waves,...
-%     'timesPerFrame',5,'axisLimits',[-50 50 -50 50 -12 20],...
-%     'startEndTime',[100 125]);
+%Plot RY response for body 1
+pos=output.bodies(1).position(:,5);
+vel=output.bodies(1).velocity(:,5);
+t=output.bodies(1).time;
+i = find(t==0);
+i2 = find(t==150);
+figure()
+plot(t(i:i2),pos(i:i2))
+xlabel('Time (s)')
+ylabel('Position (m)')
+title('Latching Position')
+
+figure()
+plot(t(i:i2),vel(i:i2))
+xlabel('Time (s)')
+ylabel('Velocity (m/s)')
+title('Latching Velocity')
+
+
+%% Calculate and Plot Power 
+
+time =  output.ptos.time;
+time2 = output.bodies(1).time;
+ii = find(time==50);
+ii2 = find(time==150);
+jj = find(time2==50);
+jj2 = find(time2==150);
+time = time(ii:ii2);
+time2 = time2(jj:jj2);
+% force = -output.ptos.forceActuation(ii:end,3);
+% vel = output.ptos.velocity(ii:end,3);
+% power = force.*vel;
+power = output.ptos.powerInternalMechanics(ii:ii2,5);
+no_latch_power = 2.3012e+03;
+%power_upper = (body.hydroForce.fExt.re(3)).^2/(8*body.hydroForce.fDamping(3,3));
+
+power_average = mean(power)
+%power_upper_average = mean(power_upper)
+
+figure
+plot(time,power)
+hold on
+yline(power_average,'-r')
+hold on
+yline(no_latch_power,'-b')
+%plot(time2,power_upper)
+xlim([25 inf])
+xlabel('Time (s)')
+ylabel('Power (W)')
+title(['body' num2str(1) ' (' output.bodies(1).name ') Power', ' -- Latching Power'])
+legend('power', 'average power','no latching power')
+
+percent_greater = power_average/no_latch_power
+
